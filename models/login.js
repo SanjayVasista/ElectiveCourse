d6e9e39c500380b5695ff88891d91d20f8db9348
SanjayChoodamani <sanjaychoodamani13@gmail.com>
2024-06-01 17:01:09 +0530

    updating index.js

--- models/login.js
+++ models/login.js
@@ -1,4 +1,7 @@
-import mysql from "mysql2"
+import mysql from "mysql2";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const pool = mysql.createPool({
     host: 'sql12.freesqldatabase.com',
@@ -9,9 +12,28 @@ const pool = mysql.createPool({
 }).promise()
 
 
-const result = await pool.query("select * from student`")
-console.log(result)
+const result = await pool.query("select * from student")
+
 result[0].map(student => {
-    console.log(student.usn)
-    console.log(student.dob.toISOString().split('T')[0])
+    let username = student.usn;
+    let dob = student.dob.toISOString().split('T')[0]; 
+    const sql = 'INSERT INTO login (username, password) VALUES (?, ?)';
+
+    bcrypt.hash(dob, saltRounds, async (err, hash)=>{
+        if(err){
+            console.error("Error while hashing ", err);
+        }else{
+            await pool.query(sql, [username, hash], (err, res)=>{
+                if(err){
+                    console.error("An error while inserting")
+                }else{
+                    console.log("Value inserted sucessfully")
+                }
+            })
+        }
+    } )
+    
+
 })
+
+export default pool;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import express from "express"
+import express from "express";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
+import mysql from "mysql2";
 
-const app = express()
-const port = 3000
+const pool = mysql.createPool({
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12707946',
+    password: 'pNL8YVacQh',
+    database: 'sql12707946',
+    timezone: 'Z'
+}).promise()
+
+const app = express();
+const port = 3000;
+
+app.use(
+    session({
+        secret: "TOPSECRETWORD",
+        resave: false,
+        saveUninitialized: true,
+        cookie:{
+            maxAge: 1000 * 60 * 5,
+        }
+    })
+);
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
 //static file path
@@ -15,30 +41,89 @@ app.get('/', async (req, res) => {
     res.render("login");
 })
 
-app.post('/login', async(req, res)=>{
-    console.log(req.body.username)
-    console.log(req.body.password)
+app.post('/login', passport.authenticate("local", {
+    successRedirect: "/branchSelection",
+    failureRedirect: "/",
+})
+)
+
+app.get('/branchSelection', async (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('branchSelection');
+    } else {
+        res.redirect('/');
+    }
 })
 
-app.get('/branchSelection', async(req, res)=>{
-    res.render('branchSelection')
+app.post('/branch', async (req, res)=>{
+    if(req.isAuthenticated()){
+        let dbBranch = req.user.username.substr(5,2); 
+        let selectBranch = req.body.branch;
+        let semester = req.body.sem;
+        
+        if(dbBranch === selectBranch){
+            res.render('subjectSelection')
+        }
+    }
+    
 })
 
-app.get('/subjectSelection', async(req, res)=>{
+app.get('/subjectSelection', async (req, res) => {
     res.render('subjectSelection')
 })
 
-app.get('/subDisplay', async(req, res)=>{
+app.get('/subDisplay', async (req, res) => {
     res.render('subDisplay')
 })
 
-app.get('/dashboard', async(req, res)=>{
+app.get('/dashboard', async (req, res) => {
     res.render('dashboardHome')
 })
 
-app.get('/creation', async(req, res)=>{
+app.get('/creation', async (req, res) => {
     res.render('dashboardCreation')
 })
+
+passport.use(
+    new Strategy(async function verify(username, password, cb) {
+        try {
+            const result = await pool.query("SELECT * FROM login WHERE username = ? ", [
+                username,
+            ]);
+            if (result[0].length > 0) {
+                const user = result[0][0];
+                const storedHashedPassword = user.password;
+                bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+                    if (err) {
+                        //Error with password check
+                        console.error("Error comparing passwords:", err);
+                        return cb(err);
+                    } else {
+                        if (valid) {
+                            //Passed password check
+                            return cb(null, user);
+                        } else {
+                            //Did not pass password check
+                            return cb(null, false);
+                        }
+                    }
+                });
+
+            } else {
+                return cb("User not found");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    })
+);
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
